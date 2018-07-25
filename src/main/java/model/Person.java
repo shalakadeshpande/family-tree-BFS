3622cf684eb4aeefbe1587aa75fc338d785121d9
shalaka <shalaka@192.168.0.107>
2018-07-25 17:10:48 +0530

    Added equals and hascode for Person

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,7 +1,6 @@
 package model;
 
 public class Person {
-	//TODO equals and hashcode override
 	private String name;
 	private String gender;
 	private String spouseName;
@@ -42,5 +41,31 @@ public class Person {
 	public String toString() {
 		return "[" + name + " | " + spouseName + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 
 }
